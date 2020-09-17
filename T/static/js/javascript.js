f6c2e37e4c558bebca3ddc77322ab8f157f661e1
mauroquinteros <quinterosmauro0599@gmail.com>
2020-09-17 16:53:08 -0500

    Adding sidebar

--- T/static/js/javascript.js
+++ T/static/js/javascript.js
@@ -7,3 +7,24 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 }
+
+const $sidebar = document.querySelector('#sidebar')
+const $sidebarBtn = document.querySelector('#sidebar-btn')
+$sidebarBtn.addEventListener('click', () => {
+  $sidebar.classList.toggle('hidden')
+})
+const $sidebarHeader = document.querySelector('#sidebar-header')
+const $childsHeader = $sidebarHeader.querySelectorAll('#sidebar-header > *')
+const $textSidebar = document.querySelectorAll('.sidebar--item-text')
+$sidebarHeader.addEventListener("transitionstart", () => {
+  if($sidebar.classList.contains('hidden')) {
+    $childsHeader.forEach(item => item.style.visibility = 'hidden')
+    $textSidebar.forEach(item => item.style.display = 'none')
+  }
+});
+$sidebarHeader.addEventListener("transitionend", () => {
+  if(!$sidebar.classList.contains('hidden')) {
+    $childsHeader.forEach(item => item.style.visibility = 'visible')
+    $textSidebar.forEach(item => item.style.display = 'inline-block')
+  }
+});

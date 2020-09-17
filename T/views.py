c583a650806da7ee99fa37c22c64afde7d212944
mauroquinteros <quinterosmauro0599@gmail.com>
2020-09-16 22:31:08 -0500

    Adding template ruta

--- T/views.py
+++ T/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def ruta(request):
+    return render(request, 'rutas.html')
+
 def reconocimiento(request):
     return render(request, 'reconocimiento.html')

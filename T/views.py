6adfc28cd9ace000347f00af56395eb7441e4d86
mauroquinteros <quinterosmauro0599@gmail.com>
2020-09-16 23:19:23 -0500

    Connect models to the database

--- T/views.py
+++ T/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from T.models import *
+from datetime import datetime
 
+import json
+
+# Llamar Templates
 def ruta(request):
-    return render(request, 'rutas.html')
+  # proyecto = obtener_proyecto()
+  # return render(request, 'rutas.html', {'proyecto': empleado})
+  return render(request, 'rutas.html')
 
 def reconocimiento(request):
-    return render(request, 'reconocimiento.html')
+  return render(request, 'reconocimiento.html')
+
+
+# Realizar consultas
+def get_empleado(): # Funcion de prueba, eliminarlo despues
+  empleado = Empleado.objects.get(dni = '77382771')
+  return empleado
+
+def obtener_proyecto():
+  proyecto = Proyecto.objects.get(codpyto = 1) # cambiar codigo segun la bd
+  return proyecto
+
+def obtener_rutas():
+  rutas = Ruta.objects.all()
+  return rutas
+
+def obtener_tramos():
+  tramos = Tramo.objects.all()
+  return tramos
